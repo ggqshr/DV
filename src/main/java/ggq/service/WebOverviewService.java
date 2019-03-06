e0c925a0d96924a4e378655701aaaaf5224b2a66
ggqshr <942490944@qq.com>
2019-03-06 09:19:50 +0800

    可以获取总体概览页的数据

--- src/main/java/ggq/service/WebOverviewService.java
+++ src/main/java/ggq/service/WebOverviewService.java
@@ -3,14 +3,18 @@ package ggq.service;
 import ggq.model.websiteOverview;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface WebOverviewService {
 
-    HashMap<String, int[]> getPvUvEtcByDataType(String dataType);
+    HashMap<String, int[]> getPvUvEtcByDataType(String subDateNumber);
 
     websiteOverview getTotal();
 
     HashMap<String,double[]> getJumpRatePerHour(String date);
 
     HashMap<String,double[]> getAvgVistiTime(String date);
+
+    List<HashMap<String,Object>> getDetailData(String date,String dataSubNumber,int currentHours);
+
 }
