ef3712fe3127561b0f3065f944e23ac1b8e77d16
ggq <ggq18663278150@sian.com>
2019-03-11 12:19:42 +0800

    添加了报表功能

--- src/main/java/ggq/controller/ProductController.java
+++ src/main/java/ggq/controller/ProductController.java
@@ -4,11 +4,23 @@ import com.alibaba.fastjson.JSON;
 import ggq.model.ActionDateShowModel;
 import ggq.model.ProductDetail;
 import ggq.service.ProductService;
+import ggq.utils.DateUtils;
+import ggq.utils.ExcelUtils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.registry.infomodel.User;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.PortUnreachableException;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +28,10 @@ import java.util.Map;
 public class ProductController {
     @Autowired
     ProductService productService;
+    @Autowired
+    ExcelUtils excelUtils;
+    @Autowired
+    DateUtils dateUtils;
 
     @RequestMapping(value = "/api/product/data", produces = "application/json; charset=utf-8")
     @ResponseBody
@@ -29,5 +45,76 @@ public class ProductController {
     public String getActionData() {
         List<ActionDateShowModel> actionData = productService.getActionData();
         return JSON.toJSONString(actionData);
+    }
+
+    @RequestMapping(value = "/api/getexcel")
+    public void getExcel(HttpServletResponse res) throws ParseException, IOException {
+        List<ActionDateShowModel> actionData = productService.getActionData();
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("用户行为分析报表");
+        HSSFRow row = null;
+        List<String> dates = dateUtils.findDates(dateUtils.getDateSubResult(-6), dateUtils.getCurrentDate());
+
+
+        CellRangeAddress rowRegion = new CellRangeAddress(0, 0, 0, 8);
+        sheet.addMergedRegion(rowRegion);
+
+        row = sheet.createRow(0);
+        row.setHeight((short) (26.25 * 20));
+        row.createCell(0).setCellValue("用户行为报表");
+        row.getCell(0).setCellStyle(excelUtils.getStyle(wb, 0));//设置样式
+        for (int i = 1; i <= 8; i++) {
+            row.createCell(i).setCellStyle(excelUtils.getStyle(wb, 0));
+        }
+
+
+//        CellRangeAddress columnRegion = new CellRangeAddress(1, 4, 0, 0);
+//        sheet.addMergedRegion(columnRegion);
+
+        row = sheet.createRow(1);
+//        row.createCell(0).setCellStyle(excelUtils.getStyle(wb, 3));
+        row.setHeight((short) (22.50 * 20));
+        row.createCell(0).setCellValue("行为ID");
+        row.createCell(1).setCellValue("行为名");
+        row.createCell(2).setCellValue(dates.get(0));
+        row.createCell(3).setCellValue(dates.get(1));
+        row.createCell(4).setCellValue(dates.get(2));
+        row.createCell(5).setCellValue(dates.get(3));
+        row.createCell(6).setCellValue(dates.get(4));
+        row.createCell(7).setCellValue(dates.get(5));
+        row.createCell(8).setCellValue(dates.get(6));
+
+        for (int i = 0; i <= 8; i++) {
+            row.getCell(i).setCellStyle(excelUtils.getStyle(wb, 1));
+        }
+
+        for (int i = 0; i < actionData.size(); i++) {
+            row = sheet.createRow(i + 2);
+            ActionDateShowModel user = actionData.get(i);
+            row.createCell(0).setCellValue(user.getAction_id());
+            row.createCell(1).setCellValue(user.getAction_name());
+            row.createCell(2).setCellValue(user.getdata0());
+            row.createCell(3).setCellValue(user.getDate1());
+            row.createCell(4).setCellValue(user.getDate2());
+            row.createCell(5).setCellValue(user.getDate3());
+            row.createCell(6).setCellValue(user.getDate4());
+            row.createCell(7).setCellValue(user.getDate5());
+            row.createCell(8).setCellValue(user.getDate6());
+            for (int j = 0; j <= 8; j++) {
+                row.getCell(j).setCellStyle(excelUtils.getStyle(wb, 2));
+            }
+        }
+
+        sheet.setDefaultRowHeight((short) (16.5 * 20));
+        //列宽自适应
+        for (int i = 0; i <= 13; i++) {
+            sheet.autoSizeColumn(i);
+        }
+
+        res.setContentType("application/vnd.ms-excel;charset=utf-8");
+        OutputStream os = res.getOutputStream();
+        wb.write(os);
+        os.flush();
+        os.close();
     }
 }
---
+++ src/main/java/ggq/utils/ExcelUtils.java
@@ -0,0 +1,60 @@
+package ggq.utils;
+
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFPalette;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExcelUtils {
+    public HSSFCellStyle getStyle(HSSFWorkbook hssfWorkbook, Integer styleNum) {
+        HSSFCellStyle style = hssfWorkbook.createCellStyle();
+        style.setBorderRight(BorderStyle.THIN);//右边框
+        style.setBorderBottom(BorderStyle.THIN);//下边框
+
+        HSSFFont font = hssfWorkbook.createFont();
+        font.setFontName("微软雅黑");//设置字体为微软雅黑
+
+        HSSFPalette palette = hssfWorkbook.getCustomPalette();//拿到palette颜色板,可以根据需要设置颜色
+        switch (styleNum) {
+            case (0): {
+                style.setAlignment(HorizontalAlignment.CENTER_SELECTION);//跨列居中
+                font.setBold(true);//粗体
+                font.setFontHeightInPoints((short) 14);//字体大小
+                style.setFont(font);
+                palette.setColorAtIndex(HSSFColor.BLUE.index, (byte) 184, (byte) 204, (byte) 228);//替换颜色板中的颜色
+                style.setFillForegroundColor(HSSFColor.BLUE.index);
+                style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            }
+            break;
+            case (1): {
+                style.setAlignment(HorizontalAlignment.CENTER);
+                font.setBold(true);//粗体
+                font.setFontHeightInPoints((short) 11);//字体大小
+                style.setFont(font);
+            }
+            break;
+            case (2): {
+                font.setFontHeightInPoints((short) 10);
+                style.setAlignment(HorizontalAlignment.CENTER);
+                style.setFont(font);
+            }
+            break;
+            case (3): {
+                style.setFont(font);
+
+                palette.setColorAtIndex(HSSFColor.GREEN.index, (byte) 0, (byte) 32, (byte) 96);//替换颜色板中的颜色
+                style.setFillForegroundColor(HSSFColor.GREEN.index);
+                style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            }
+            break;
+        }
+
+        return style;
+    }
+}
